3e177f694afa30193c99ca6f3268a1ef40423319
thyrdmc <mtyardimci45@gmail.com>
2023-04-28 12:04:31 +0300

    Added Caesar Cipher Program & Updated Other Configurations

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,9 +4,6 @@ print(caesar_cipher_logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-
-
 def caesar_cipher(text, shift, direction):
     new_text = ''
     shift %=26
