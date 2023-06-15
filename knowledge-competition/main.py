92ff7795a4db26e9a61d2452260895f4f0b27a20
thyrdmc <mtyardimci45@gmail.com>
2023-06-15 14:05:43 +0300

    Added Py-Millionaire Knowledge Competetion

---
+++ knowledge-competition/main.py
@@ -0,0 +1,22 @@
+from question_model import Question
+from data import question_data
+from question_machine import  QuestionMachine
+
+# TODO 2: Creating the List of Question Objects from the Data  and Saving into Question Pool
+
+question_pool = []
+
+for data in question_data:
+
+    text = data['question']
+    answer = data['correct_answer']
+    question_ = Question(text, answer)
+    question_pool.append(question_)
+
+question_machine_ = QuestionMachine(question_pool)
+
+while question_machine_.any_unused_question():
+    question_machine_.next_question()
+
+print("You've completed the competiton!")
+print(f"Your Final Score was {question_machine_.score}/{len(question_pool)}.")
---
+++ knowledge-competition/question_machine.py
@@ -0,0 +1,33 @@
+# TODO 3: Creating a Question Machine Class
+# Question Machine Class Responsibilities : Asking the questions | Checking if the answer was correct | Checking is game over
+
+class QuestionMachine:
+    def __init__(self, question_list):
+        self.question_list = question_list
+        self.question_number = 0
+        self.score = 0
+
+# TODO 4: Creating methods for Question Machine Class
+    def any_unused_question(self):
+        if self.question_number < len(self.question_list):
+            return True
+        else:
+            return False
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        correct_answer = current_question.answer
+        self.question_number += 1
+        user_answer = input(f"Question {self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, correct_answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("\t Congratulations, Keep going on!")
+        else:
+            print("\t Oh No, It was wrong answer.")
+            print(f"\t The correct answer was {correct_answer}.")
+
+        print(f"The current score is {self.score}/{self.question_number}.")
+        print("\n")
